d36e51f17cd06702782bde60ab8a623ef401f8b4
silicao3o <zergskybmw@gmail.com>
2024-07-11 01:22:08 +0900

    feat#5 : UserEntity & Timestamped & RoleEnum & StatusEnum 생성

--- src/main/java/com/sparta/team15/entity/User.java
+++ src/main/java/com/sparta/team15/entity/User.java
@@ -1,5 +1,48 @@
 package com.sparta.team15.entity;
 
-public class User {
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "user")
+public class User extends Timestamped{
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private long id;
+
+  private String username;
+
+  private String name;
+
+  private String password;
+
+  @Enumerated(value = EnumType.STRING)
+  private UserStatusEnum status;
+
+  @Enumerated(value = EnumType.STRING)
+  private UserRoleEnum role;
+
+  private String createdAt;
+
+  private String modifiedAt;
+
+  public User(String username, String password, String name, UserRoleEnum role) {
+    this.username = username;
+    this.password = password;
+    this.name = name;
+    this.status = UserStatusEnum.USER;
+    this.role = role;
+  }
 
 }

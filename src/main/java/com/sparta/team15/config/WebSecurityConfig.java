86621d7f7bc2496dfcdd948384f1289b95ede6bb
fenec_fox <atimevil@gmail.com>
2024-07-11 10:37:29 +0900

    feat : PasswordEncoder Bean

--- src/main/java/com/sparta/team15/config/WebSecurityConfig.java
+++ src/main/java/com/sparta/team15/config/WebSecurityConfig.java
@@ -1,10 +1,17 @@
 package com.sparta.team15.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-
-
-
+@Configuration
 public class WebSecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 
 }

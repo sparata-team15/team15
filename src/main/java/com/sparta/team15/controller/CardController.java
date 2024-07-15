9436fb8d358f791ddfe3d6a79c0507eb32072d05
haze <rmstn4233@gmail.com>
2024-07-15 23:24:38 +0900

    커밋

--- src/main/java/com/sparta/team15/controller/CardController.java
+++ src/main/java/com/sparta/team15/controller/CardController.java
@@ -1,5 +1,4 @@
 package com.sparta.team15.controller;
-
 import com.sparta.team15.dto.CardRequestDto;
 import com.sparta.team15.dto.CardResponseDto;
 import com.sparta.team15.dto.ResponseMessageDto;
@@ -11,6 +10,7 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,28 +20,22 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
-
 @RestController
 @RequestMapping("/cards")
 public class CardController {
-
     private final CardService cardService;
-
     public CardController(CardService cardService) {
         this.cardService = cardService;
     }
-
     // 카드 생성
     @PostMapping
     public ResponseEntity<ResponseMessageDto> createCard(
-        @Valid @RequestBody CardRequestDto requestDto,
-        @AuthenticationPrincipal UserDetailsImpl userDetails) {
+            @Valid @RequestBody CardRequestDto requestDto,
+            @AuthenticationPrincipal UserDetailsImpl userDetails) {
         cardService.createCard(requestDto, userDetails);
         return ResponseEntity.ok(new ResponseMessageDto(MessageEnum.CARD_CREATED));
     }
-
     // 카드 전체 목록 조회
     @GetMapping
     public List<CardResponseDto> getAllCards(@RequestParam @Min(1) int page,
@@ -49,7 +43,6 @@ public class CardController {
                                              @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return cardService.getAllCards(page-1, size, userDetails);
     }
-
     // 카드 상태별 조회
     @GetMapping("/status/{columnId}")
     public List<CardResponseDto> getCardsByStatus(@PathVariable Long columnId,
@@ -60,39 +53,49 @@ public class CardController {
     }
 
     // 카드 작업자별 조회
-    @GetMapping("/{author}")
+    @GetMapping("/author/{author}")
     public List<CardResponseDto> getCardsByAuthor(@PathVariable String author,
                                                   @RequestParam @Min(1) int page,
                                                   @RequestParam @Min(1) @Max(5) int size,
                                                   @AuthenticationPrincipal UserDetailsImpl userDetails) {
         return cardService.getCardsByAuthor(author, page-1, size, userDetails);
     }
-
     // 카드 수정
     @PutMapping("/{cardId}")
     public ResponseEntity<ResponseMessageDto> updateCard(@PathVariable Long cardId,
-        @RequestBody CardRequestDto requestDto,
-        @AuthenticationPrincipal UserDetailsImpl userDetails) {
+                                                         @RequestBody CardRequestDto requestDto,
+                                                         @AuthenticationPrincipal UserDetailsImpl userDetails) {
         cardService.updateCard(cardId, requestDto, userDetails);
         return ResponseEntity.ok(new ResponseMessageDto(MessageEnum.CARD_UPDATED));
     }
-
     // 카드 삭제
     @DeleteMapping("/{cardId}")
     public ResponseEntity<ResponseMessageDto> updateCard(@PathVariable Long cardId,
-        @AuthenticationPrincipal UserDetailsImpl userDetails) {
+                                                         @AuthenticationPrincipal UserDetailsImpl userDetails) {
         cardService.deleteCard(cardId, userDetails);
         return ResponseEntity.ok(new ResponseMessageDto(MessageEnum.CARD_DELETED));
     }
-
     // 카드 순서 이동
     @PutMapping("/position/{cardId}")
     public ResponseEntity<ResponseMessageDto> updateCardPosition(@PathVariable Long cardId,
-        @RequestBody CardRequestDto requestDto,
-        @AuthenticationPrincipal UserDetailsImpl userDetails) {
+                                                                 @RequestBody CardRequestDto requestDto,
+                                                                 @AuthenticationPrincipal UserDetailsImpl userDetails) {
         cardService.updateCardPosition(cardId, requestDto, userDetails);
         return ResponseEntity.ok(new ResponseMessageDto(MessageEnum.CARD_UPDATE_POSITION));
     }
 
+    /*// 카드 단일 조회
+    @GetMapping("/{cardId}")
+    public List<CardResponseDto> getCard(@PathVariable Long cardId, @AuthenticationPrincipal UserDetailsImpl userDetails) {
+        return cardService.getCard(cardId, userDetails);
+    }*/
+
+    // 카드 단일 조회
+    @GetMapping("/{cardId}")
+    public String getCard(@PathVariable Long cardId, @AuthenticationPrincipal UserDetailsImpl userDetails, Model model) {
+        CardRequestDto card = cardService.getCard(cardId,userDetails);
+        model.attribute("card",card);
+        return "cardsdetail";
+    }
 
 }
--- src/main/java/com/sparta/team15/service/CardService.java
+++ src/main/java/com/sparta/team15/service/CardService.java
@@ -1,5 +1,4 @@
 package com.sparta.team15.service;
-
 import com.sparta.team15.dto.CardRequestDto;
 import com.sparta.team15.dto.CardResponseDto;
 import com.sparta.team15.entity.BoardColumn;
@@ -19,112 +18,92 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class CardService {
-
     private final CardRepository cardRepository;
     private final BoardColumnRepository boardColumnRepository;
     private final BoardUserRepository boardUserRepository;
-
     // 카드 생성
     public void createCard(CardRequestDto requestDto, UserDetailsImpl userDetails) {
         User user = userDetails.getUser();
         BoardColumn boardColumn = findBoardColumn(requestDto.getColumnId());
-
         //쿼리 최적화 추가
         boolean isExistBoardUser = boardUserRepository
                 .existsByUserAndBoard(user, boardColumn.getBoard());
         if (!isExistBoardUser) {
             throw new NotFoundException(BoardColumnErrorCode.NOT_TEAM_MEMBER);
         }
-
         Card card = new Card(
-            user,
-            requestDto.getAuthor(),
-            boardColumn,
-            requestDto.getContent(),
-            requestDto.getDescription(),
-            requestDto.getDate()
+                user,
+                requestDto.getAuthor(),
+                boardColumn,
+                requestDto.getContent(),
+                requestDto.getDescription(),
+                requestDto.getDate()
         );
         cardRepository.save(card);
     }
-
     // 카드 전체 목록 조회
     public List<CardResponseDto> getAllCards(int page, int size, UserDetailsImpl userDetails) {
-
         Pageable pageable = PageRequest.of(page, size);
-
         return cardRepository.findAll(pageable).stream()
-            .map(CardResponseDto::new).toList();
+                .map(CardResponseDto::new).toList();
     }
-
     // 카드 상태별 조회
     public List<CardResponseDto> getCardsByStatus(Long columnId, int page, int size, UserDetailsImpl userDetails) {
         BoardColumn boardColumn = findBoardColumn(columnId);
-
         Pageable pageable = PageRequest.of(page, size);
-
         return cardRepository.findByBoardColumn(boardColumn, pageable).stream()
-            .map(CardResponseDto::new).toList();
+                .map(CardResponseDto::new).toList();
     }
-
     // 카드 작업자별 조회
     public List<CardResponseDto> getCardsByAuthor(String author, int page, int size, UserDetailsImpl userDetails) {
-
         Pageable pageable = PageRequest.of(page, size);
-
         return cardRepository.findByAuthor(author, pageable).stream()
-            .map(CardResponseDto::new).toList();
+                .map(CardResponseDto::new).toList();
     }
-
     // 카드 수정
     public void updateCard(Long cardId, CardRequestDto requestDto, UserDetailsImpl userDetails) {
         User user = userDetails.getUser();
         Card card = findCard(cardId);
-
-
         if (card.getUser().getId() != user.getId()) {
             throw new AuthorizedException(UserErrorCode.NOT_AUTHORIZATION_ABOUT_CARD);
         }
-
         card.update(requestDto.getAuthor(), requestDto.getContent(), requestDto.getDescription(),
-            requestDto.getDate());
+                requestDto.getDate());
         cardRepository.save(card);
-
     }
-
     // 카드 삭제
     public void deleteCard(Long cardId, UserDetailsImpl userDetails) {
         User user = userDetails.getUser();
-
         Card card = findCard(cardId);
-
         if (card.getUser().getId() != user.getId()) {
             throw new AuthorizedException(UserErrorCode.NOT_AUTHORIZATION_ABOUT_CARD);
         }
-
         cardRepository.delete(card);
     }
-
     // 카드 순서 이동
     public void updateCardPosition(Long cardId, CardRequestDto requestDto, UserDetailsImpl userDetails) {
-
         Card card = findCard(cardId);
         card.updatePosition(requestDto.getPosition());
         cardRepository.save(card);
     }
-
     private BoardColumn findBoardColumn(Long columnId) {
         return boardColumnRepository.findById(columnId)
                 .orElseThrow(() -> new NotFoundException(BoardColumnErrorCode.NOT_FOUND_COLUMN));
     }
-
     private Card findCard(Long cardId) {
         return cardRepository.findById(cardId)
                 .orElseThrow(() -> new NotFoundException(UserErrorCode.NOT_FOUND_CARD));
+    }
+
+    public CardResponseDto getCard(Long cardId, UserDetailsImpl userDetails) {
+        findCard(cardId);
+
+        return cardRepository.findById(cardId).stream()
+                .map(CardResponseDto::new).toList();
     }
 
 }
